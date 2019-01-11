0eb95e13c9ca25f40a2d99df5fcc3659c5b18e31
Alex Yuly <alexyuly2@gmail.com>
2019-01-10 22:50:12 -0800

    Work on ideas for an example

--- src/index.js
+++ src/index.js
@@ -2,57 +2,85 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import Box from './Box'
+import Media from './Media'
 import Type from './Type'
 
-const example = (
+const Titlebar = () => (
+  <Box
+    height={50}
+    layout='across'
+    style='titlebar'
+  >
+    <Type>
+      My App
+    </Type>
+  </Box>
+)
+
+const Sidebar = () => (
+  <Box
+    layout='down'
+    style='sidebar'
+    width={({ widescreen }) => widescreen && 300}
+  >
+    <Type>
+      Hi, I'm the "sidebar"! You might add some navigation content to me.
+    </Type>
+  </Box>
+)
+
+const MainContent = () => (
+  <Box
+    layout='down'
+    style='mainContent'
+  >
+    <Type>
+      Hi, I'm the "main content"! You may add whatever you like.
+    </Type>
+    <Media>
+      {({ widescreen }) => widescreen && (
+        <Type>
+          This text is only visible on screens with a min width of 800!
+        </Type>
+      )}
+    </Media>
+  </Box>
+)
+
+const Example = () => (
   <Box
     layout='rows'
     withMedia={{
       widescreen: {
-        screen: true,
         minWidth: 800,
+        screen: true,
+      },
+    }}
+    withStyles={{
+      titlebar: {
+        backgroundColor: '#222222',
+        color: '#FFFFFF',
+        fontSize: 20,
+        fontWeight: 'bold',
+      },
+      sidebar: {
+        backgroundColor: '#CCCCCC',
+        color: '#111111',
+      },
+      mainContent: {
+        backgroundColor: '#FFFFFF',
+        color: '#111111',
       },
     }}
   >
+    <Titlebar />
     <Box
-      height={50}
-      layout='across'
+      layout={({ widescreen }) => (widescreen ? 'columns' : 'down')}
     >
-      <Type>
-        My App
-      </Type>
-    </Box>
-    <Box
-      media={({ widescreen }) => ({
-        layout: widescreen ? 'columns' : 'down',
-      })}
-    >
-      <Box
-        layout='down'
-        media={({ widescreen }) => ({
-          width: widescreen && 300,
-        })}
-      >
-        <Type>
-          Hi, I'm the "sidebar"! You might add some navigation content to me.
-        </Type>
-      </Box>
-      <Box
-        layout='down'
-      >
-        <Type>
-          Hi, I'm the "main content"! You may add whatever you like.
-        </Type>
-        <Media>
-          {({ widescreen }) => widescreen && (
-            <Type>
-              This text is only visible on screens with a min width of 800!
-            </Type>
-          )}
-        </Media>
-      </Box>
+      <Sidebar />
+      <MainContent />
     </Box>
   </Box>
 )
 
-ReactDom.render(example, document.body.appendChild(document.createElement('div')))
+ReactDom.render(<Example />, document.body.appendChild(document.createElement('div')))
